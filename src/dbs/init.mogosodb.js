09be65c4b03a216ef848a50eec85e80fcabbf70c
SDG 258 <nsdu.17ck1@gmail.com>
2023-09-15 18:26:05 +0700

    Add file .env

--- src/dbs/init.mogosodb.js
+++ src/dbs/init.mogosodb.js
@@ -1,8 +1,10 @@
 'use strict'
 
 const mongoose = require('mongoose')
+const { db: { host, name , port }} = require('../configs/config.mongodb')
 
-const connectString = `mongodb://127.0.0.1:27017/shopDEV`
+const connectString = `mongodb://${host}:${port}/${name}`
+console.log(connectString)
 const { countConnect } = require('../helpers/check.connect')
 
 class Database {
